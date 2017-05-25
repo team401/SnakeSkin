af95f3957fb6f4c1a40d4ea7f3845c0469ef30b9
Zach <zachkozar@masterzach32.net>
2017-05-25 11:42:02 -0400

    Fixed button mappings

--- src/main/kotlin/org/team401/snakeskin/io/hardware/LogitechDualAction.kt
+++ src/main/kotlin/org/team401/snakeskin/io/hardware/LogitechDualAction.kt
@@ -23,9 +23,9 @@ class LogitechDualAction(dsPort: Int) : InputDevice(dsPort), Gamepad {
 
     override fun getB() = getButtonValue(2)
 
-    override fun getX() = getButtonValue(3)
+    override fun getX() = getButtonValue(0)
 
-    override fun getY() = getButtonValue(4)
+    override fun getY() = getButtonValue(3)
 
     override fun getLeftTrigger() = object : Range {
         override fun read() = if (getButtonValue(6).isTriggered()) 1.0 else 0.0
@@ -35,25 +35,25 @@ class LogitechDualAction(dsPort: Int) : InputDevice(dsPort), Gamepad {
         override fun read() = if (getButtonValue(7).isTriggered()) 1.0 else 0.0
     }
 
-    override fun getLeftBumper() = getButtonValue(5)
+    override fun getLeftBumper() = getButtonValue(4)
 
-    override fun getRightBumper() = getButtonValue(6)
+    override fun getRightBumper() = getButtonValue(5)
 
     override fun getLeftX() = getAxis(0)
 
     override fun getLeftY() = getAxis(1).scale(-1.0)
 
-    override fun getLeftStick() = getButtonValue(9)
+    override fun getLeftStick() = getButtonValue(10)
 
     override fun getRightX() = getAxis(4)
 
     override fun getRightY() = getAxis(5).scale(-1.0)
 
-    override fun getRightStick() = getButtonValue(10)
+    override fun getRightStick() = getButtonValue(11)
 
-    override fun getStart() = getButtonValue(8)
+    override fun getStart() = getButtonValue(9)
 
-    override fun getSelect() = getButtonValue(7)
+    override fun getSelect() = getButtonValue(8)
 
     override fun getDPad() = getDPadValue(0)
 
--- src/main/kotlin/org/team401/snakeskin/io/hardware/XBox360.kt
+++ src/main/kotlin/org/team401/snakeskin/io/hardware/XBox360.kt
@@ -6,7 +6,7 @@ import org.team401.snakeskin.io.RumbleGamepad
 import org.team401.snakeskin.logic.scale
 
 /*
- * SnakeSkin - Created on 5/24/17
+ * SnakeSkin - Created on 5/25/2017
  * Author: Zachary Kozar
  * 
  * This code is licensed under the GNU GPL v3
@@ -15,41 +15,41 @@ import org.team401.snakeskin.logic.scale
 
 /**
  * @author Zachary Kozar
- * @version 5/24/17
+ * @version 5/25/2017
  */
-class LogitechF310(dsPort: Int) : InputDevice(dsPort), RumbleGamepad {
+class XBox360(dsPort: Int) : InputDevice(dsPort), RumbleGamepad {
 
-    override fun getA() = getButtonValue(0)
+    override fun getA() = getButtonValue(1)
 
-    override fun getB() = getButtonValue(1)
+    override fun getB() = getButtonValue(2)
 
-    override fun getX() = getButtonValue(2)
+    override fun getX() = getButtonValue(3)
 
-    override fun getY() = getButtonValue(3)
+    override fun getY() = getButtonValue(4)
 
     override fun getLeftTrigger() = getAxis(2)
 
     override fun getRightTrigger() = getAxis(3)
 
-    override fun getLeftBumper() = getButtonValue(4)
+    override fun getLeftBumper() = getButtonValue(5)
 
-    override fun getRightBumper() = getButtonValue(5)
+    override fun getRightBumper() = getButtonValue(6)
 
     override fun getLeftX() = getAxis(0)
 
     override fun getLeftY() = getAxis(1).scale(-1.0)
 
-    override fun getLeftStick() = getButtonValue(8)
+    override fun getLeftStick() = getButtonValue(9)
 
     override fun getRightX() = getAxis(4)
 
     override fun getRightY() = getAxis(5).scale(-1.0)
 
-    override fun getRightStick() = getButtonValue(9)
+    override fun getRightStick() = getButtonValue(10)
 
-    override fun getStart() = getButtonValue(7)
+    override fun getStart() = getButtonValue(8)
 
-    override fun getSelect() = getButtonValue(6)
+    override fun getSelect() = getButtonValue(7)
 
     override fun getDPad() = getDPadValue(0)
 
