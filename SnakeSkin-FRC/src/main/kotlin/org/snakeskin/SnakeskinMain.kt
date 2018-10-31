186037c83c945923ca10fadf8646e3442096bd80
Cameron <camearle20@users.noreply.github.com>
2018-10-31 13:43:46 -0400

    Added new main class to support WPILib's new robot init system

---
+++ SnakeSkin-FRC/src/main/kotlin/org/snakeskin/SnakeskinMain.kt
@@ -0,0 +1,16 @@
+package org.snakeskin
+
+import edu.wpi.first.wpilibj.RobotBase
+
+/**
+ * @author Cameron Earle
+ * @version 10/31/2018
+ *
+ * Main class used by the new version of WPILib
+ */
+object SnakeskinMain {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        RobotBase.startRobot { Robot() }
+    }
+}
