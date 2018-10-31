ba74e7667665f62d45feef0c8ec8a0ec46825da8
Cameron <camearle20@users.noreply.github.com>
2018-10-30 22:53:31 -0400

    Added directional indices

--- SnakeSkin-Core/src/main/kotlin/org/snakeskin/logic/Direction.kt
+++ SnakeSkin-Core/src/main/kotlin/org/snakeskin/logic/Direction.kt
@@ -14,4 +14,26 @@ object Direction {
     const val SOUTHWEST = 225
     const val WEST = 270
     const val NORTHWEST = 315
+
+    /**
+     * Returns the vertical index of the given direction.  This is 1.0 for up, -1.0 for down, and 0.0 for any other direction
+     */
+    fun verticalIndex(value: Int): Double {
+        when (value) {
+            NORTH -> return 1.0
+            SOUTH -> return -1.0
+        }
+        return 0.0
+    }
+
+    /**
+     * Returns the horizontal index of the given direction.  This is 1.0 for right, -1.0 for left, and 0.0 for any other direction
+     */
+    fun horizontalIndex(value: Int): Double {
+        when (value) {
+            EAST -> return 1.0
+            WEST -> return -1.0
+        }
+        return 0.0
+    }
 }
