d7416791e1e020f2d36542249b79925042fa543d
Cameron <camearle20@users.noreply.github.com>
2020-01-07 18:59:37 -0500

    Changed RealTimeStateActionManager to use the "register" function instead of init

--- SnakeSkin-Core/src/main/kotlin/org/snakeskin/state/RealTimeStateActionManager.kt
+++ SnakeSkin-Core/src/main/kotlin/org/snakeskin/state/RealTimeStateActionManager.kt
@@ -4,14 +4,14 @@ import org.snakeskin.measure.time.TimeMeasureSeconds
 import org.snakeskin.rt.RealTimeTask
 import org.snakeskin.runtime.SnakeskinRuntime
 
-class RealTimeStateActionManager(private val rtRunnable: (TimeMeasureSeconds, TimeMeasureSeconds) -> Unit, executorName: String? = null): IStateActionManager {
+class RealTimeStateActionManager(private val rtRunnable: (TimeMeasureSeconds, TimeMeasureSeconds) -> Unit, val executorName: String? = null): IStateActionManager {
     private val rtTask = object : RealTimeTask(false) {
         override fun action(timestamp: TimeMeasureSeconds, dt: TimeMeasureSeconds) {
             rtRunnable(timestamp, dt)
         }
     }
 
-    init {
+    override fun register() {
         SnakeskinRuntime.registerRealTimeTask(rtTask, executorName)
     }
 
